5ba21b118e7618916b9bbe876617325dda24d456
Mohamed Ali <midobahebrasouly@gmail.com>
2026-02-16 16:06:01 +0200

    making two receive functions one for normla commands and one for data

--- Gateways/ITA/commands_ip.py
+++ Gateways/ITA/commands_ip.py
@@ -1,22 +1,24 @@
 import asyncio
 import re
-from CDSR import send, receive
-
+from CDSR import send , receive_data , receive_normal
 
 def ip():
-    ip = input("enter ip: ") or "192.168.13.10"
-    if re.match(r"^(\d{1,3}\.){3}\d{1,3}$", ip) or not any(
-        int(part) > 255 for part in ip.split(".")
-    ):
+    ip = input("enter ip") or '192.168.13.10'
+    if  re.match(r'^(\d{1,3}\.){3}\d{1,3}$',ip) or not any(int(part) > 255 for part in ip.split('.')) :
         return ip
     raise ValueError("Not a valid ip")
+         
 
+async def excute_command(reader , writer , command):
+    await send(writer,command)
+    resp = await receive_normal(reader)
+    if 'NA' in resp['response'] or 'ER' in resp['response'] :
+        raise ValueError(f"Error with {command} command as the response is {resp['response']}")
+    return resp
 
-async def excute_command(reader, writer, command):
-    await send(writer, command)
-    resp = await receive(reader)
-    if "NA" in resp or "ER" in resp:
-        raise ValueError(
-            f"Error with {command} command as the response is {resp['response']}"
-        )
+async def excute_data_command(reader , writer , command, TL = 0):
+    await send(writer,command)
+    resp = await receive_data(reader,command,TL)
+    if 'NA' in resp['response'] or 'ER' in resp['response'] :
+        raise ValueError(f"Error with {command} command as the response is {resp['response']}")
     return resp
--- Gateways/ITA/main-3.py
+++ Gateways/ITA/main-3.py
@@ -1,42 +1,36 @@
 import asyncio
-from commands_ip import ip, excute_command
+from commands_ip import ip , excute_command ,excute_data_command
 from parameters_con import parameters
-from csv_des import take_A_decision, csvf
-from CDSR import connect, disconnect
-
+from csv_des import take_A_decision , csvf
+from CDSR import connect , disconnect 
 
 async def main():
     port = 8020
-    # timeout = 15
     reader = None
     writer = None
-    intetvals_second = 5
 
-    used_ip = ip() or "192.168.13.10"
+
+    used_ip = ip() or '192.168.13.10'
     used_parameters = parameters()
     try:
-        reader, writer = await connect(used_ip, port)
-        x = await excute_command(reader, writer, "FV?")
+        reader , writer = await connect(used_ip ,port )
+        x=await excute_command(reader,writer,'FV?')
         print(x)
-        x = await excute_command(reader, writer, "DS?")
-        print(x)
-        x = await excute_command(reader, writer, used_parameters["commands"])
-        print(x)
-        # while True:
-        #     x = await excute_command(reader, writer, "AQ")
-        #     print(x)
-        #     data = await excute_command(reader, writer, "BD?", used_parameters["TL"])
-        #     csvf(data)
-        #     await take_A_decision(reader, writer)
-        await asyncio.sleep(intetvals_second)
+        y=await excute_command(reader,writer,'DS?')
+        print(y)
+        z= await excute_command(reader,writer,used_parameters['commands'])
+        print(z)
+        xx=await excute_command(reader,writer,'AQ')
+        print(xx)
+        data =await excute_data_command(reader,writer,'BD?',used_parameters['TL'])
+        csvf(data)
     except KeyboardInterrupt:
         print("( ctrl+c ) stops the code.")
     except Exception as e:
         print(f"Error :{e}")
-    finally:
+    finally: 
         await disconnect(writer)
         print("Disconnected successfully")
-
 
 if __name__ == "__main__":
     try:
